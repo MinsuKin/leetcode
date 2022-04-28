600abc3d843bcf7c62af9fa85a0c978f88d7dcd0
Minsu Kim <minkim@c3r9p2.42quebec.com>
2022-04-28 15:57:40 -0400

    04.28

---
+++ 155. Min Stack.py
@@ -0,0 +1,57 @@
+
+
+# # My code
+# class MinStack:
+
+#     def __init__(self):
+#         self.stack = []
+#         self.min = []
+
+#     def push(self, val: int) -> None:
+#         if self.min == []:
+#             self.min.append(val)
+#         elif val < self.min[-1]:
+#             self.min.append(val)
+#         elif val >= self.min[-1]:
+#             self.min.append(self.min[-1])
+#         self.stack.append(val)
+
+#     def pop(self) -> None:
+#         if self.stack == []:
+#             return None
+#         else:
+#             self.stack.pop()
+#             self.min.pop()
+
+#     def top(self) -> int:
+#         if self.stack == []:
+#             return None
+#         return self.stack[-1]
+
+#     def getMin(self) -> int:
+#         if self.min == []:
+#             return None
+#         else:
+#             return self.min[-1]
+
+
+
+# getMin not working as O(1)
+# class MinStack:
+
+#     def __init__(self):
+#         self.stack = []
+
+#     def push(self, val: int) -> None:
+#         self.stack.append(val)
+
+#     def pop(self) -> None:
+#         self.stack.pop()
+
+#     def top(self) -> int:
+#         if self.stack == []:
+#             return None
+#         return self.stack[-1]
+
+#     def getMin(self) -> int:
+#         return min(self.stack)
