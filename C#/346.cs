24dc2897512f837c435c506f5f8e2a1a45b246b6
Minsu Kim <minkim@c3r9p2.42quebec.com>
2024-03-03 14:45:30 -0500

    Moving Average from Data Stream

--- C#/346.cs
+++ C#/346.cs
@@ -24,3 +24,32 @@ public class MovingAverage {
         return totalSum / numbers.Count;
     }
 }
+
+
+// Queue 사용 (index로 element 접근불가)
+public class MovingAverage
+{
+    private int maxSize;
+    private double totalSum;
+    private Queue<int> numbers;
+
+    public MovingAverage(int size)
+    {
+        maxSize = size;
+        totalSum = 0.0; // double로 초기화
+        numbers = new Queue<int>();
+    }
+
+    public double Next(int val)
+    {
+        if (numbers.Count == maxSize)
+        {
+            totalSum -= numbers.Dequeue();
+        }
+
+        totalSum += val;
+        numbers.Enqueue(val);
+
+        return totalSum / numbers.Count;
+    }
+}
