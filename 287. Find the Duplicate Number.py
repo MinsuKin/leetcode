86059095870cca09efb0bb75624d454b493281d9
Minsu Kim <minkim@c3r9p2.42quebec.com>
2024-03-03 14:26:27 -0500

    Moving Average from Data Stream

--- 287. Find the Duplicate Number.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def findDuplicate(self, nums: List[int]) -> int:
-        length = len(nums)
-        nums.sort()
-        for i in range(1, length):
-            if nums[i] == nums[i-1]:
-                return nums[i]
